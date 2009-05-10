3891f9e733b33984698a2df639018bf36598db21
Zachary Voase <disturbyte@gmail.com>
2009-05-11 00:53:02 +0200

    Added benchmark.py so that people can see how I got those ridiculous benchmarks I've been getting.

---
+++ benchmark.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# It's a far cry from 'elegant', but it gets the job done.
+
+import time
+from zenqueue import client
+
+MESSAGES = 1000000 # 1,000,000 (1 million) messages will be sent in total.
+UNIT_SIZE = 10000 # Each request will contain 10,000 (10 thousand) messages.
+UNIT = [u'a'] * UNIT_SIZE
+
+# Assumes that the server is running on localhost:3000
+c = client.QueueClient()
+
+start_time = time.time()
+
+for i in xrange(MESSAGES / UNIT_SIZE):
+    c.push_many(*tuple(UNIT))
+
+for j in xrange(MESSAGES / UNIT_SIZE):
+    assert (c.pull_many(UNIT_SIZE, timeout=0) == UNIT)
+
+end_time = time.time()
+
+c.close()
+
+time_taken = end_time - start_time
+
+print '%d Messages transferred' % (MESSAGES,)
+print 'Time taken: %0.4f seconds' % (time_taken,)
+print 'Average: %0.4f messages/second' % (MESSAGES / time_taken,)
