8509216b88bc8d6ac52f3431e5fd5d9dbeaaef5d
Zachary Voase <disturbyte@gmail.com>
2009-05-13 01:04:52 +0200

    Finally gave in and switched to setuptools. Bumped to v0.4.1

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
 
 
 setup(
     name='ZenQueue',
-    version='0.4',
+    version='0.4.1',
     description='An incredibly simple (but fast) network message queueing system, written in Python.',
     author='Zachary Voase',
     author_email='disturbyte@gmail.com',
     url='http://github.com/disturbyte/zenqueue',
-    packages=['zenqueue']
+    packages=find_packages(),
+    install_requires=['simplejson'],
 )
